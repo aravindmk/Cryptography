7f33a5b0cd41c217bbc1a91c2c7936f55dcf3485
d4rk3n <aravindmk@outlook.com>
2020-09-22 19:12:16 +0530

    Add files via upload

---
+++ hex to base64.py
@@ -0,0 +1,4 @@
+from binascii import b2a_base64,unhexlify
+a='49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+b=b2a_base64(unhexlify(a)) #Decoding hex and encoding it to base64
+print(b[0:len(b)-1])   #Indexing the string to remove escape sequence
