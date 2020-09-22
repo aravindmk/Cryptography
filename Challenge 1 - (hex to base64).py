7add60cf7dbaf585b9c63633eeb57da7c63e076f
d4rk3n <aravindmk@outlook.com>
2020-09-22 19:18:12 +0530

    Update and rename hex to base64.py to Challenge 1 - (hex to base64).py

--- Challenge 1 - (hex to base64).py
+++ Challenge 1 - (hex to base64).py
@@ -1,3 +1,5 @@
+#Hex to Basee64 convert
+
 from binascii import b2a_base64,unhexlify
 a='49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 b=b2a_base64(unhexlify(a)) #Decoding hex and encoding it to base64
