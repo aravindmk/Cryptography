4c4843a4c7779efa5d9955640f067b4c98b15c9b
d4rk3n <aravindmk@outlook.com>
2020-09-23 07:59:33 +0530

    Update and rename Single byte Xor.py to Challenge 3 - (Single byte Xor).py

--- Challenge 3 - (Single byte Xor).py
+++ Challenge 3 - (Single byte Xor).py
@@ -2,8 +2,7 @@
 
 from binascii import unhexlify
 hx = unhexlify('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
-print(hx)
 for key in range(256):
-    decoded = ''.join(chr(b ^ key) for b in hx)
+    decoded = ''.join(chr(b ^ key) for b in hx) #Doin xor on each byte of the decoded bytes
     if decoded.isprintable():
         print(key, decoded)
